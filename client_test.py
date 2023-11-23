9e5f9129cfd38c41bcdfadd646c3edb8b73adb79
lamfey <38252165+lamfey@users.noreply.github.com>
2023-11-23 11:20:27 +0000

    TASK 1 AND BONUS TASK COMPLETED

--- client_test.py
+++ client_test.py
@@ -19,7 +19,7 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
+    """ ------------ Add the assertion below ------------- """
     for quote in quotes:
       stock, bid_price, ask_price, price = getDataPoint(quote)
       expected_price = (quote['top_bid']['price'] + quote['top_ask']['price']) / 2
